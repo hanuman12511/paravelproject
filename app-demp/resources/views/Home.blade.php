651ff3e851ebc55ed9cdb62ecb7a797c809e6fc6
hanuman12511 <er.hanumansaini@gmail.com>
2024-01-07 17:25:12 +0530

    pro

---
+++ app-demp/resources/views/Home.blade.php
@@ -0,0 +1,6 @@
+
+
+@extends('layouts.main')
+@section('main-section')
+<h1>Home</h1>
+@endsection
---
+++ app-demp/resources/views/page.blade.php
@@ -0,0 +1,5 @@
+
+@extends('layouts.main')
+@section('main-section')
+<h1>page well</h1>
+@endsection
